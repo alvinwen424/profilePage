0e10b6b4a883d8aecd6aacfff4337371fe6a4287
Alvin Wen <alvinwen424@gmail.com>
2017-09-13 17:33:51 -0400

    initial routes and index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const app = express();
+
+app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, './path/to/static/assets')));
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('api', require('./routes'))
+
+
